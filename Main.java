2f808f0bd2b365779f0bf722ad7959d833cce5e0
Omar <61245842+Omar228228@users.noreply.github.com>
2024-03-26 12:24:22 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Обчислення множення чисел 54 і 47
+        int result = multiply(54, 47);
+        
+        // Виведення результату у консоль
+        System.out.println("Результат множення 54 на 47: " + result);
+    }
+    
+    // Метод для множення двох чисел
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
